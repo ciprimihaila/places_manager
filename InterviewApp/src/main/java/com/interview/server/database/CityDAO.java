79bc23800e72d1348f3e1dac2529c22da4c6d624
Ciprian Mihaila <cipri.mihaila@gmail.com>
2017-08-19 15:28:19 +0300

    update folder name

--- InterviewApp/src/main/java/com/interview/server/database/CityDAO.java
+++
@@ -1,9 +0,0 @@
-package com.interview.server.database;
-
-import com.interview.shared.City;
-
-public interface CityDAO {
-	public void saveCity(City city);
-
-	public City findCity(String cityName);
-}
--- InterviewApp/src/main/java/com/interview/server/database/PhotoReferenceDAO.java
+++
@@ -1,9 +0,0 @@
-package com.interview.server.database;
-
-import java.util.List;
-
-import com.interview.shared.PhotoReference;
-
-public interface PhotoReferenceDAO {
-	public List<PhotoReference> getPhotosForPlaces(String placeID);
-}
